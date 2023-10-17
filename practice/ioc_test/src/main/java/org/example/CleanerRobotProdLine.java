c510a0c95d3c95b9db33bb99e871d4ca04d6d654
Mag1strVark <molot2101@gmail.com>
2023-10-17 15:05:49 +0500

    remove practice

--- practice/ioc_test/src/main/java/org/example/CleanerRobotProdLine.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class CleanerRobotProdLine implements RoboProductionLine{
-    @Override
-    public void work() {
-        System.out.println("new CLEANER robot build completed");
-    }
-}
